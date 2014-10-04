60c10f2a301e131eace7a12fdb6ccdaf5bb09c50
balapuri <balapuri@gmail.com>
2014-10-04 11:12:19 -0700

    added root navigation and first scene

---
+++ Capsule/CapsuleCell.h
@@ -0,0 +1,16 @@
+//
+//  CapsuleCell.h
+//  Capsule
+//
+//  Created by Maya Reddy on 10/4/14.
+//  Copyright (c) 2014 Mask. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CapsuleCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *mainPicture;
+@property (weak, nonatomic) IBOutlet UIImageView *smallPicture;
+
+@property (weak, nonatomic) IBOutlet UILabel *captionLabel;
+@end
---
+++ Capsule/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  Capsule
+//
+//  Created by Maya Reddy on 10/4/14.
+//  Copyright (c) 2014 Mask. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
