a62bd5a0fd3311e182809aa74cfea068e7d5512b
Andy Li <acli@berkeley.edu>
2014-10-05 00:48:16 +0800

    Init commit from local

---
+++ Capsule/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Capsule
+//
+//  Created by Andy Li on 10/5/14.
+//  Copyright (c) 2014 Mask. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
