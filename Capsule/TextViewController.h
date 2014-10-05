330b33d79a24de8cf8a628ac866eeec862bacc9f
balapuri <balapuri@gmail.com>
2014-10-04 20:03:21 -0700

    added view controller for adding text

---
+++ Capsule/TextViewController.h
@@ -0,0 +1,15 @@
+//
+//  TextViewController.h
+//  Capsule
+//
+//  Created by Sneha Sankavaram on 10/4/14.
+//  Copyright (c) 2014 Mask. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextViewController : UIViewController
+- (IBAction)saveText:(id)sender;
+- (IBAction)cancelText:(id)sender;
+
+@end
